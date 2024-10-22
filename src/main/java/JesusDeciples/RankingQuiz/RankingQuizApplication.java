9fcecdc29d1fcbde840c37633d7b0322cd68d5d9
RiverCastle <rkdtjd12@gmail.com>
2024-10-22 11:26:47 +0900

    first commit

---
+++ src/main/java/JesusDeciples/RankingQuiz/RankingQuizApplication.java
@@ -0,0 +1,13 @@
+package JesusDeciples.RankingQuiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RankingQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RankingQuizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/JesusDeciples/RankingQuiz/RankingQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package JesusDeciples.RankingQuiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankingQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
