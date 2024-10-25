3b4f1efdb4febeafe8f0c2ad8b46ccc2bcacac90
RiverCastle <rkdtjd12@gmail.com>
2024-10-26 00:26:24 +0900

    feat: WebSocketQuizHandler add
    - WebSocketQuizHandler를 추가하여 퀴즈 기능을 위한 WebSocket 연결 처리.
    - 퀴즈 상태 관리(대기, 진행 중, 채점 완료) 구현.
    - 정기적으로 퀴즈 상태를 확인하고 전환하는 스케줄링 작업 통합.
    - 퀴즈 결과 및 새 퀴즈 메시지를 클라이언트에게 전송하는 로직 추가.
    - 클라이언트와의 원활한 통신을 위한 오류 처리 포함.

---
+++ src/main/java/JesusDeciples/RankingQuiz/api/webSocket/WebSocketQuizHandler.java
@@ -0,0 +1,119 @@
+package JesusDeciples.RankingQuiz.api.webSocket;
+
+import JesusDeciples.RankingQuiz.api.dto.response.QuizResultDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.*;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.*;
+
+import static JesusDeciples.RankingQuiz.api.webSocket.QuizSystemState.*;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class WebSocketQuizHandler {
+    private final Long waitingTime = 3L; // 퀴즈 수거 대기 시간
+    private final CustomTextMessageFactory textMessageFactory;
+    private QuizSystemState presentState = WAITING;
+    private final QuizDataCenter quizDataCenter;
+    private final Map<String, WebSocketSession> sessions = new HashMap<>();
+
+    @Scheduled(fixedDelay = 1000)
+    private void checkPresentState() throws IOException {
+
+        switch (presentState) {
+            case ON_QUIZ -> {
+                log.info("퀴즈 진행중 채점 곧 시작함");
+                log.info("CURRENT QUIZ ID = " + quizDataCenter.getPresentQuiz().getId());
+                LocalDateTime quizFinishedAt = quizDataCenter.getPresentQuizFinishedAt();
+                LocalDateTime now = LocalDateTime.now();
+                if (now.isAfter(quizFinishedAt)) {
+                    log.info("퀴즈 종료 - 답안 수집 시작");
+                    updateQuizSystemState(COMPLETED_QUIZ_GETTING_ANSWERED);
+                }
+                break;
+            }
+            case COMPLETED_QUIZ_GETTING_ANSWERED -> {
+                log.info("답안 수집 진행 중");
+                LocalDateTime collectingAnswersFinishedAt = quizDataCenter.getPresentQuizFinishedAt().plusSeconds(waitingTime);
+                LocalDateTime now = LocalDateTime.now();
+                if (now.isAfter(collectingAnswersFinishedAt)) {
+                    log.info("정답 수거 종료 채점 시작");
+                    updateQuizSystemState(ON_SCORING);
+                }
+                break;
+            }
+        }
+    }
+
+    public void updateQuizSystemState(QuizSystemState nextState) throws IOException {
+        if (this.presentState != nextState) {
+            if (presentState == ON_QUIZ & nextState == COMPLETED_QUIZ_GETTING_ANSWERED) {
+                log.info("현재 상태 = " + presentState +" 다음 상태 = " + nextState);
+                this.presentState = nextState; // 퀴즈 중 -> 퀴즈 종료, 답안 수거로 상태 변경
+            }
+            else if (presentState == COMPLETED_QUIZ_GETTING_ANSWERED & nextState == ON_SCORING) {
+                log.info("현재 상태 = " + presentState +" 다음 상태 = " + nextState);
+                this.presentState = nextState; // 퀴즈 종료, 답안 수거 -> 채점 시작
+                quizDataCenter.score();
+                updateQuizSystemState(COMPLETED_SCORING);
+            }
+            else if (presentState == ON_SCORING & nextState == COMPLETED_SCORING) {
+                log.info("현재 상태 = " + presentState +" 다음 상태 = " + nextState);
+                this.presentState = nextState; // 채점 시작 -> 채점 종료
+                Set<String> sessionIds = sessions.keySet();
+                Map<String, QuizResultDto> results = quizDataCenter.getResults();
+                Set<String> sessionIdsOfParticipants = results.keySet();
+                for (String sessionId : sessionIds) {
+                    if (sessionIdsOfParticipants.contains(sessionId)) {
+                        TextMessage textMessage = new TextMessage( // QuizResult -> TextMessage 변환 및 전송
+                                textMessageFactory.produceTextMessage(results.get(sessionId)));
+                        sessions.get(sessionId).sendMessage(textMessage);
+                    } else {
+                        sessions.get(sessionId).sendMessage(new TextMessage("퀴즈에 참가하지 않으셨어요. 다음 퀴즈에는 꼭 참가해보세요!"));
+                    }
+                }
+                updateQuizSystemState(ON_QUIZ_SETTING);
+            }
+            else if (presentState == COMPLETED_SCORING & nextState == ON_QUIZ_SETTING) {
+                log.info("현재 상태 = " + presentState +" 다음 상태 = " + nextState);
+                // 채점 종료 -> 새 퀴즈 생성
+                // 전체 세션에 새 퀴즈 전송
+                this.presentState = nextState;
+                quizDataCenter.setNewQuizExcept(); // 새 퀴즈 생성
+                String newQuizTextMessage = // 새 퀴즈 메시지로 변환
+                        textMessageFactory.produceTextMessage(quizDataCenter.getPresentQuizDto());
+
+                Set<String> sessionIds = sessions.keySet();
+                for (String sessionId : sessionIds)
+                    sessions.get(sessionId).sendMessage(new TextMessage(newQuizTextMessage));
+                updateQuizSystemState(ON_QUIZ);
+            }
+            else if (presentState == WAITING & nextState == ON_QUIZ_SETTING) {
+                log.info("현재 상태 = " + presentState +" 다음 상태 = " + nextState);
+                // 대기 상태 -> 퀴즈 처음 시작
+                // 전체 세션에 새 퀴즈 메시지 전송
+                log.info("퀴즈 최초 시작");
+                this.presentState = nextState;
+                quizDataCenter.initiateQuiz(); // 퀴즈를 처음 시작
+                String newQuizTextMessage = // 새 퀴즈 메시지로 변환
+                        textMessageFactory.produceTextMessage(quizDataCenter.getPresentQuizDto());
+
+                // 모든 세션에게 퀴즈 전송
+                Set<String> sessionIds = sessions.keySet();
+                for (String sessionId : sessionIds)
+                    sessions.get(sessionId).sendMessage(new TextMessage(newQuizTextMessage));
+                updateQuizSystemState(ON_QUIZ);
+            }
+            else if (presentState == ON_QUIZ_SETTING & nextState == ON_QUIZ) {
+                log.info("현재 상태 = " + presentState +" 다음 상태 = " + nextState);
+                this.presentState = ON_QUIZ;
+            }
+        }
+    }
+}
