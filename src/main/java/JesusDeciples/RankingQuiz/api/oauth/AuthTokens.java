559a95b3e72eeedfe7e13e08882730b2e6076817
RiverCastle <rkdtjd12@gmail.com>
2024-11-01 23:16:54 +0900

    Fix: AuthTokens 기능 수정
    - Authorization 권한 부여 추가
    - refresh token 제거
    - access token 과 같은 메서드를 사용하여 권한파라미터가 들어가는데, refresh token 무관하다고 생각함.

--- src/main/java/JesusDeciples/RankingQuiz/api/oauth/AuthTokens.java
+++ src/main/java/JesusDeciples/RankingQuiz/api/oauth/AuthTokens.java
@@ -9,11 +9,10 @@ import lombok.Setter;
 @AllArgsConstructor
 public class AuthTokens {
     private String accessToken;
-    private String refreshToken;
     private String grantType;
     private Long expiresIn;
 
-    public static AuthTokens of(String accessToken, String refreshToken, String grantType, Long expiresIn) {
-        return new AuthTokens(accessToken, refreshToken, grantType, expiresIn);
+    public static AuthTokens of(String accessToken, String grantType, Long expiresIn) {
+        return new AuthTokens(accessToken, grantType, expiresIn);
     }
 }
