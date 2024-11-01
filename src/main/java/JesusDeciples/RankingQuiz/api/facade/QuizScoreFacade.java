b55ce6e4d9b4c9157173cebd5c8eeeb18648f661
RiverCastle <rkdtjd12@gmail.com>
2024-11-02 08:50:17 +0900

    Refactor: 변수명 수정
    - finishedQuiz -> quiz
    - answer -> userAnswer

--- src/main/java/JesusDeciples/RankingQuiz/api/facade/QuizScoreFacade.java
+++ src/main/java/JesusDeciples/RankingQuiz/api/facade/QuizScoreFacade.java
@@ -2,7 +2,11 @@ package JesusDeciples.RankingQuiz.api.facade;
 
 import JesusDeciples.RankingQuiz.api.dto.AnswerDto;
 import JesusDeciples.RankingQuiz.api.dto.response.QuizResultDto;
+import JesusDeciples.RankingQuiz.api.entity.Member;
+import JesusDeciples.RankingQuiz.api.entity.QuizResult;
 import JesusDeciples.RankingQuiz.api.entity.quiz.Quiz;
+import JesusDeciples.RankingQuiz.api.service.MemberService;
+import JesusDeciples.RankingQuiz.api.service.QuizResultService;
 import JesusDeciples.RankingQuiz.api.service.quiz.QuizService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -13,16 +17,17 @@ public class QuizScoreFacade {
 //    추후에 score 결과 save를 위해 facade 적용
     private final QuizService quizService;
     public QuizResultDto score(Long quizId, AnswerDto answerDto) {
-        Quiz finishedQuiz = quizService.getQuiz(quizId);
-        String answer = answerDto.getUserAnswer();
-        boolean isCorrect = finishedQuiz.getQuizContent().getAnswer().equals(answer);
+        Quiz quiz = quizService.getQuiz(quizId);
+        String userAnswer = answerDto.getUserAnswer();
+        boolean isCorrect = quiz.getQuizContent().getAnswer().equals(userAnswer);
+        
         QuizResultDto dto = new QuizResultDto(isCorrect);
-        dto.setMyAnswer(answer);
-        dto.setAnswer(finishedQuiz.getQuizContent().getAnswer());
-        dto.setStatement(finishedQuiz.getQuizContent().getStatement());
+        dto.setMyAnswer(userAnswer);
+        dto.setAnswer(quiz.getQuizContent().getAnswer());
+        dto.setStatement(quiz.getQuizContent().getStatement());
         dto.setWrittenAt(answerDto.getWrittenAt());
         dto.setUserName(answerDto.getUserName());
-        // TODO score save logic
+
         return dto;
     }
 }
