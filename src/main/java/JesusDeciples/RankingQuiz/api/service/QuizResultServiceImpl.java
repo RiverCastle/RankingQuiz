7d4782b804a4ce69caa42051675577c7b7dc960a
RiverCastle <rkdtjd12@gmail.com>
2024-11-02 16:48:30 +0900

    Feat: 퀴즈 결과 조회 API 추가
    - 조회 기능 실제 구현내용

--- src/main/java/JesusDeciples/RankingQuiz/api/service/QuizResultServiceImpl.java
+++ src/main/java/JesusDeciples/RankingQuiz/api/service/QuizResultServiceImpl.java
@@ -1,12 +1,18 @@
 package JesusDeciples.RankingQuiz.api.service;
 
+import JesusDeciples.RankingQuiz.api.dto.response.QuizResultDto;
 import JesusDeciples.RankingQuiz.api.entity.Member;
 import JesusDeciples.RankingQuiz.api.entity.QuizResult;
 import JesusDeciples.RankingQuiz.api.entity.quiz.Quiz;
+import JesusDeciples.RankingQuiz.api.entity.quizContent.QuizContent;
 import JesusDeciples.RankingQuiz.api.repository.QuizResultRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -21,5 +27,34 @@ public class QuizResultServiceImpl implements QuizResultService {
         quizResult.setMyAnswer(userAnswer);
         quizResult.setWrittenAt(writtenAt);
         return quizResultRepository.save(quizResult);
+    }
+
+    @Override
+    @Transactional
+    public List<QuizResultDto> getMyQuizResults(Long memberId) {
+        List<QuizResult> results = quizResultRepository.findAllByMemberId(memberId);
+        List<QuizResultDto> resultDtos = new ArrayList<>();
+        for (QuizResult result : results) {
+            if (result.isCorrect()) continue; // 틀린 문제만 결과 제공
+            QuizContent quizContent = result.getQuiz().getQuizContent();
+            String answer = quizContent.getAnswer();
+            String myAnswer = result.getMyAnswer();
+            boolean haveAnswered = true;
+            if (myAnswer == null) {
+                myAnswer = "없음";
+                haveAnswered = true;
+            }
+
+            QuizResultDto resultDto = new QuizResultDto();
+            resultDto.setCorrect(answer.equals(myAnswer));
+            resultDto.setStatement(quizContent.getStatement());
+            resultDto.setAnswer(answer);
+            resultDto.setMyAnswer(myAnswer);
+            resultDto.setHaveAnswered(haveAnswered);
+            resultDto.setWrittenAt(result.getWrittenAt());
+
+            resultDtos.add(resultDto);
+        }
+        return resultDtos;
     }
 }
