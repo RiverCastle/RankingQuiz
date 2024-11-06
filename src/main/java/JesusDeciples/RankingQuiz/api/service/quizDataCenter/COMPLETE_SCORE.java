b6b2c05c6e9fcfa982848fb6bd6666fa9e09c687
RiverCastle <rkdtjd12@gmail.com>
2024-11-06 14:05:36 +0900

    Refactor: QuizDataCenter State Pattern
    - COMPLETE_SCORE 상태 구현체 생성
    - 웹소켓 핸들러에서 퀴즈 결과 전송 동작을 위한 Flag 역할
    - DataCenter에 요구되는 동작 없음

---
+++ src/main/java/JesusDeciples/RankingQuiz/api/service/quizDataCenter/COMPLETE_SCORE.java
@@ -0,0 +1,10 @@
+package JesusDeciples.RankingQuiz.api.service.quizDataCenter;
+
+import JesusDeciples.RankingQuiz.api.webSocket.QuizDataCenter;
+
+public class COMPLETE_SCORE implements DataCenterState {
+    @Override
+    public void handle(QuizDataCenter quizDataCenter) {
+
+    }
+}
