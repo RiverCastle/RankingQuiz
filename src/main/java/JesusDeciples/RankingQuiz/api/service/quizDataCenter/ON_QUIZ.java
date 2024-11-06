87d78755f659758820387f4e43aeeefffbb1c8aa
RiverCastle <rkdtjd12@gmail.com>
2024-11-06 14:00:39 +0900

    Refactor: QuizDataCenter State Pattern
    - ON_QUIZ 상태 구현체 생성
    - 퀴즈 종료 여부 확인 동작
    - COMPLETE_QUIZ 상태가 되기 전까지 WebSocketQuizHandler에서 1초 주기로 호출

---
+++ src/main/java/JesusDeciples/RankingQuiz/api/service/quizDataCenter/ON_QUIZ.java
@@ -0,0 +1,21 @@
+package JesusDeciples.RankingQuiz.api.service.quizDataCenter;
+
+import JesusDeciples.RankingQuiz.api.webSocket.QuizDataCenter;
+
+import java.time.LocalDateTime;
+
+public class ON_QUIZ implements DataCenterState {
+    private LocalDateTime deadline;
+    @Override
+    public void handle(QuizDataCenter quizDataCenter) {
+        if (!(quizDataCenter.getPresentState() instanceof ON_QUIZ)) {
+            quizDataCenter.setPresentState(this);
+        }
+        if (deadline == null) {
+            deadline = quizDataCenter.getPresentQuizFinishedAt().minusSeconds(2);
+        }
+        if (LocalDateTime.now().isAfter(deadline)) {
+            quizDataCenter.setPresentState(new COMPLETE_QUIZ());
+        }
+    }
+}
