a5712fd93c561035b3b966b625ff8261865a5b70
RiverCastle <rkdtjd12@gmail.com>
2024-11-13 21:52:46 +0900

    Feat: 제출시간 서버에서 측정하도록 변경

--- src/main/java/JesusDeciples/RankingQuiz/api/webSocket/AnswerDtoMessageHandler.java
+++ src/main/java/JesusDeciples/RankingQuiz/api/webSocket/AnswerDtoMessageHandler.java
@@ -1,10 +1,12 @@
 package JesusDeciples.RankingQuiz.api.webSocket;
 
 import JesusDeciples.RankingQuiz.api.dto.AnswerDto;
-import JesusDeciples.RankingQuiz.api.service.quizDataCenter.VocaQuizDataCenter;
+import JesusDeciples.RankingQuiz.api.service.quizDataCenter.voca.VocaQuizDataCenter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 @Component
 @RequiredArgsConstructor
@@ -16,6 +18,7 @@ public class AnswerDtoMessageHandler {
         Long presentQuizId = vocaQuizDataCenter.getPresentQuizDto().getQuizId();
         AnswerDto answerDto = objectMapper.convertValue(objectInMessage, AnswerDto.class);
         Long quizIdInAnswerDto = answerDto.getQuizId();
+        answerDto.setWrittenAt(LocalDateTime.now());
         if (!presentQuizId.equals(quizIdInAnswerDto)) return; // 현재 퀴즈와 무관한 답안
 
         // 엑세스 토큰을 가진 세션의 경우 memberId 값이 있음
