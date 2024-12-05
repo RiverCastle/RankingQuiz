9a4a6805d74ab910d7a917a20d27f9e6e473d71d
RiverCastle <rkdtjd12@gmail.com>
2024-12-06 02:03:34 +0900

    Feat: 학습자 맞춤 복습 기능 추가
    - 객관식 문항 Entity 생성 로직 추가
    - 테스트 케이스 추가 (리팩토링 필요)

--- src/main/java/JesusDeciples/RankingQuiz/api/entity/quizContent/MultipleChoiceQuizContent.java
+++ src/main/java/JesusDeciples/RankingQuiz/api/entity/quizContent/MultipleChoiceQuizContent.java
@@ -1,5 +1,6 @@
 package JesusDeciples.RankingQuiz.api.entity.quizContent;
 
+import JesusDeciples.RankingQuiz.api.dto.request.QuizContentCreateDto;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -12,4 +13,19 @@ import java.util.List;
 public class MultipleChoiceQuizContent extends QuizContent {
     @ElementCollection(fetch = FetchType.EAGER)
     private List<String> options;
+
+    public static MultipleChoiceQuizContent of(QuizContentCreateDto dto) {
+        if (dto.getStatement() == null) throw new RuntimeException("문제가 없습니다.");
+        if (dto.getAnswer() == null) throw new RuntimeException("정답이 없습니다.");
+        if (dto.getCategory() == null) throw new RuntimeException("카테고리를 설정해주세요.");
+        if (!dto.getMultipleOptions().contains(dto.getAnswer())) throw new RuntimeException("정답이 포함되어 있지 않습니다.");
+
+        MultipleChoiceQuizContent entity = new MultipleChoiceQuizContent();
+        entity.setOptions((dto).getMultipleOptions());
+        entity.setStatement(dto.getStatement());
+        entity.setAnswer(dto.getAnswer());
+        entity.setTimeLimit(dto.getTimeLimit());
+        entity.setCategory(dto.getCategory());
+        return entity;
+    }
 }
--- src/main/java/JesusDeciples/RankingQuiz/api/service/MultipleChoiceQuizContentServiceImpl.java
+++ src/main/java/JesusDeciples/RankingQuiz/api/service/MultipleChoiceQuizContentServiceImpl.java
@@ -6,17 +6,22 @@ import JesusDeciples.RankingQuiz.api.repository.MultipleChoiceQuizContentReposit
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class MultipleChoiceQuizContentServiceImpl implements MultipleChoiceQuizContentService {
     private final MultipleChoiceQuizContentRepository repository;
     @Override
     public MultipleChoiceQuizContent createMultipleChoiceQuizContent(QuizContentCreateDto dto) {
-        return null;
+        List<String> multipleOptions = dto.getMultipleOptions();
+        if (multipleOptions == null) throw new RuntimeException("보기가 필요합니다. 보기를 추가해주세요.");
+        else if (multipleOptions.size() <= 1) throw new RuntimeException("보기의 항목 수가 너무 적습니다. 현재 개수 = " + multipleOptions.size());
+        return MultipleChoiceQuizContent.of(dto);
     }
 
     @Override
     public void saveToRepository(MultipleChoiceQuizContent entity) {
-
+        repository.save(entity);
     }
 }
