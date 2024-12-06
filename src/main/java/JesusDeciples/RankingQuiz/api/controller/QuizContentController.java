b85c06a0b8cbc803ab681383c2ec9bd24337e8eb
RiverCastle <rkdtjd12@gmail.com>
2024-12-06 10:36:44 +0900

    Feat: 학습자 맞춤 복습 기능 추가
    - 복습 내용 요청 엔드포인트

--- src/main/java/JesusDeciples/RankingQuiz/api/controller/QuizContentController.java
+++ src/main/java/JesusDeciples/RankingQuiz/api/controller/QuizContentController.java
@@ -1,9 +1,14 @@
 package JesusDeciples.RankingQuiz.api.controller;
 
+import JesusDeciples.RankingQuiz.api.dto.QuizContentDto;
 import JesusDeciples.RankingQuiz.api.dto.request.QuizContentCreateDto;
 import JesusDeciples.RankingQuiz.api.facade.QuizContentCreateFacade;
+import JesusDeciples.RankingQuiz.api.facade.QuizContentReadFacade;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,9 +16,15 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = {"http://localhost:8081", "https://rankingquiz.rivercastleworks.site"})
 public class QuizContentController {
     private final QuizContentCreateFacade quizContentCreateFacade;
+    private final QuizContentReadFacade readFacade;
     @PostMapping
     public void addQuizContent(@RequestBody QuizContentCreateDto[] quizContentCreateDtos) {
         for (QuizContentCreateDto quizContentCreateDto : quizContentCreateDtos)
             quizContentCreateFacade.createQuizContent(quizContentCreateDto);
+    }
+
+    @GetMapping("/review-content/{quizContentId}")
+    public ResponseEntity<List<QuizContentDto>> getQuizContentForReview(@PathVariable("quizContentId") Long quizContentId) {
+        return ResponseEntity.ok(readFacade.readQuizContentLinkedWith(quizContentId));
     }
 }
