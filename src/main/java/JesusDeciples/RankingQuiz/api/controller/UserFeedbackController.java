ec01eff38d50430126df6e6e4fb2b85b2ca6300d
RiverCastle <rkdtjd12@gmail.com>
2024-12-11 16:29:38 +0900

    Docs: UserFeedbackController

--- src/main/java/JesusDeciples/RankingQuiz/api/controller/UserFeedbackController.java
+++ src/main/java/JesusDeciples/RankingQuiz/api/controller/UserFeedbackController.java
@@ -3,6 +3,8 @@ package JesusDeciples.RankingQuiz.api.controller;
 import JesusDeciples.RankingQuiz.api.dto.UserFeedbackDto;
 import JesusDeciples.RankingQuiz.api.service.userFeedback.UserFeedbackService;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,10 +12,13 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/api/user-feedback")
 @CrossOrigin(origins = {"http://localhost:8081", "https://rankingquiz.rivercastleworks.site"})
+@Tag(name = "UserFeedback API", description = "서비스 이용 후기 API 입니다.")
 public class UserFeedbackController {
     private final UserFeedbackService userFeedbackService;
 
     @PostMapping
+    @Operation(summary = "사용자 피드백 제출",
+            description = "사용자의 서비스 이용 후기를 GitHub Issue API를 사용해 Repository Issues에 등록합니다.")
     public void postUserFeedback(@RequestBody UserFeedbackDto[] userFeedbackDtos) throws JsonProcessingException {
         for (UserFeedbackDto dto : userFeedbackDtos)
             userFeedbackService.postFeedback(dto);
