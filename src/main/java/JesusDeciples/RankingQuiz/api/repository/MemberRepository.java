ca55281dd9842458ba666945d613f7249a44e61c
RiverCastle <rkdtjd12@gmail.com>
2024-12-19 14:56:51 +0900

    Fix: Member Point 적립 동시성 문제 해결 테스트 추가
    - 점수 적립을 위한 조회 시 비관적 락 사용

--- src/main/java/JesusDeciples/RankingQuiz/api/repository/MemberRepository.java
+++ src/main/java/JesusDeciples/RankingQuiz/api/repository/MemberRepository.java
@@ -1,11 +1,18 @@
 package JesusDeciples.RankingQuiz.api.repository;
 
 import JesusDeciples.RankingQuiz.api.entity.Member;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT m FROM Member m WHERE m.id = :id")
+    Optional<Member> findByIdForUpdate(Long id);
 }
